7938f97356ef1ec8b3fb6e7193985e91777d753e
pree11 <47377140+pree11@users.noreply.github.com>
2019-02-25 16:26:24 +0530

    Create br11.java

---
+++ br11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hello
+{
+	public static void main(String ar[])
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		  int b=in.nextInt();
+		System.out.println(Math.round(Math.pow(a, b))); 
+		
+	}
+}
