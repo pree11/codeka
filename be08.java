fe3b3310c41934eaefc9df5f566c78a670394ad1
pree11 <47377140+pree11@users.noreply.github.com>
2019-02-25 16:11:28 +0530

    Create be08.java

---
+++ be08.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class hello
+{
+	public static void main(String ar[])
+	{
+		Scanner n=new Scanner(System.in);
+		int m=n.nextInt();
+		int sum=0; 
+		for(int i=1;i<=m;i++)
+		{
+sum=sum+i;
+		}
+System.out.println(sum);
+		
+	}
+}
