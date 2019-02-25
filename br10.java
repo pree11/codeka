3992d0449f5d7f0d7709ff11cb39a6e4f387bd96
pree11 <47377140+pree11@users.noreply.github.com>
2019-02-25 16:23:25 +0530

    Create br10.java

---
+++ br10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hello
+{
+	public static void main(String ar[])
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		 String str1 = Integer.toString(n);
+		System.out.print(str1.length());
+		
+	}
+}
