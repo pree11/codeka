77ff81afaee7aefc8732bb6f94a4499d9c39f88b
pree11 <47377140+pree11@users.noreply.github.com>
2019-02-25 16:08:29 +0530

    Create be07.java

---
+++ be07.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class hello
+{
+	public static void main(String ar[])
+	{
+		Scanner n=new Scanner(System.in);
+		int m=n.nextInt();
+		for(int i=0;i<m;i++)
+		{
+		System.out.println("Hello");
+		}
+	}
+}
