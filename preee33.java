66eaa6432bb672abbfdce8128670d53ca8ec2757
pree11 <47377140+pree11@users.noreply.github.com>
2019-03-07 09:46:25 -0800

    Create preee33.java

---
+++ preee33.java
@@ -0,0 +1,24 @@
+public class SpaceCount
+{
+    
+    static int i,c=0,res;
+    
+    
+    static int spacecount(String s)
+    {
+        for(i=0,c=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch==' ')
+            c++;
+        }
+        return c;
+    }
+    
+    public static void main (String args[])
+    {
+        res=SpaceCount.spacecount("i am adult ");
+       
+        System.out.println("The number of white spaces  in the String are :  "+res);
+    }
+}
