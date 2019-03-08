d0cf8c1280b9c9f0b081c68deee3b4346412743f
pree11 <47377140+pree11@users.noreply.github.com>
2019-03-08 13:43:15 +0530

    Create power50.java

---
+++ power50.java
@@ -0,0 +1,21 @@
+ import java.util.*;
+       public class power_of_two
+{  
+    public static void main(String args[])  
+    {  
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = in.nextInt();
+
+        int other = 4;  
+        if(((~num) & 4) == 4)  
+        {  
+            System.out.println("The number is a power of two");  
+        }  
+        else  
+        {
+            System.out.println("The number is a  NOT A power of two");  
+        }
+    }  
+} 
