3ae9ece5e9d7ef2186faf56114f0cf3da85c5a43
pree11 <47377140+pree11@users.noreply.github.com>
2019-03-08 12:01:00 +0530

    Create pree44.c

---
+++ pree44.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(n==i)
+        count=1;
+    }
+    if(count==1)
+    
+        printf("yes");
+        else
+        printf("no");
+    
+    
+
+    return 0;
+}
