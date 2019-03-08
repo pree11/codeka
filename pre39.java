10e743bf50a6d26efe625d6da6ced5e2623390dd
pree11 <47377140+pree11@users.noreply.github.com>
2019-03-08 10:59:10 +0530

    Create pre39.java

---
+++ pre39.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+    public class Largest_Number 
+    {
+
+        public static void main(String[] args) 
+
+        {
+
+            int n, max;
+
+            Scanner s = new Scanner(System.in);
+
+            System.out.print("Enter number of elements in the array:");
+
+            n = s.nextInt();
+
+            int a[] = new int[n];
+
+            System.out.println("Enter elements of array:");
+
+            for(int i = 10; i < n; i++)
+
+            {
+
+                a[i] = s.nextInt();
+
+            }
+
+            max = a[10];
+
+            for(int i =10; i < n; i++)
+
+            {
+
+                if(max < a[i])
+
+                {
+
+                    max = a[i];
+
+                }
+
+            }
+
+            System.out.println("Maximum value:"+max);
+        }
+        
+        
+        
+        }
+            
+            
